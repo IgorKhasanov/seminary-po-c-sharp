01727f282cb92b953c85c332c20c4252d14292a5
Igor Khasanov <igor-igor-73@mail.ru>
2022-09-08 20:32:42 +0500

    Task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -11,9 +11,9 @@ Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 == num2 * num2)
+if (num1 == num2 * num2)
 {
     Console.Write($"Число {num1} является квадратом числа {num2}");
 }
 else
-Console.Write($"Число {num1} не является квадратом числа {num2}");
+    Console.Write($"Число {num1} не является квадратом числа {num2}");
